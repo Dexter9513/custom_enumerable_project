2539b58fdb1090751ed3231d51ffdf3951cd95b0
Dexter <dextermorgan9513@gmail.com>
2022-06-14 11:34:16 +0545

    for loop instead of upto

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,49 +1,45 @@
 module Enumerable
   # Your code goes here
   def my_each
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       yield value
     end
     self
   end
-  
+
   def my_each_with_index
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    i = 0
+    for value in self do
       yield value, i
+      i += 1
     end
     self
   end
 
   def my_select
     to_return = []
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       to_return << value if yield value
     end
     to_return
   end
 
   def my_all?
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       return false unless yield value
     end
     true
   end
 
   def my_any?
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       return true if yield value
     end
     false
   end
 
   def my_none?
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       return false if yield value
     end
     true
@@ -51,20 +47,16 @@ module Enumerable
 
   def my_count
     return self.length unless block_given?
-    i = 0
     count = 0
-    self.length.times do
-      value = self[i]
+    for value in self do
       count += 1 if yield value
-      i -= 1
     end
     count
   end
 
   def my_map
     to_return = []
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       to_return << yield(value)
     end
     to_return
@@ -72,8 +64,7 @@ module Enumerable
 
   def my_inject(initial)
     to_return = initial
-    0.upto(self.length - 1) do |i|
-      value = self[i]
+    for value in self do
       to_return = yield to_return, value
     end
     to_return
